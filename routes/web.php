68340bce4ccea97ba255195ac4b195be39cdb4da
Helder Emiliano de Souza <helder@mobiliadecor.com.br>
2026-03-24 11:41:07 -0300

    feat:Ajustes nas rotas do painel principal, (remover o /helder)

--- routes/web.php
+++ routes/web.php
@@ -5,7 +5,7 @@ use App\Http\Controllers\MercadoLivreAuthController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect('/login');
 });
 
 // Bling OAuth (sem middleware auth para permitir reautorizacao direta em producao)
