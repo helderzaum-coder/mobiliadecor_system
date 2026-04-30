79afd84a4754c2b93ca65ba18b3068e54494f3b5
Helder <helder.emiliano@hotmail.com>
2026-04-30 14:37:01 -0300

    fix: adicionar role marketing ao canAccessPanel para permitir acesso ao painel

--- app/Models/User.php
+++ app/Models/User.php
@@ -52,6 +52,6 @@ class User extends Authenticatable implements FilamentUser
 
     public function canAccessPanel(Panel $panel): bool
     {
-        return $this->hasAnyRole(['admin', 'operador', 'tampo']);
+        return $this->hasAnyRole(['admin', 'operador', 'tampo', 'marketing']);
     }
 }
