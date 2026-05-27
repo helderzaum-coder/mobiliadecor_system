417c1cda4ec7f29f3af45ff055802a8075f7df26
Helder <helder.emiliano@hotmail.com>
2026-05-27 09:46:53 -0300

    feat: agendar VariacaoTamposJob a cada 6 horas via schedule

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -3,11 +3,15 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Console\Scheduling\Schedule;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withCommands([
         __DIR__.'/../app/Console/Commands',
     ])
+    ->withSchedule(function (Schedule $schedule) {
+        $schedule->job(new \App\Jobs\VariacaoTamposJob('primary'))->everySixHours();
+    })
     ->withRouting(
         web: __DIR__.'/../routes/web.php',
         commands: __DIR__.'/../routes/console.php',
