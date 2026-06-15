b4b2e041fe505ecfdb494c390271bedad7791cf5
Helder <helder.emiliano@hotmail.com>
2026-06-15 20:40:24 -0300

    style: forçar fundo cinza sólido nas linhas da tabela realtime ML

--- resources/views/filament/pages/listagem-anuncios-ml.blade.php
+++ resources/views/filament/pages/listagem-anuncios-ml.blade.php
@@ -67,7 +67,7 @@
                                     $isFirst = $loop->first;
                                     $borderTop = $isFirst && $upIdx > 0 ? 'border-top:3px solid #3b82f6;' : '';
                                 @endphp
-                                <tr class="border-b border-gray-100 dark:border-gray-700/50" style="background:rgba(55,65,81,0.4);{{ $borderTop }}">
+                                <tr style="background:#374151 !important;{{ $borderTop }}" class="border-b border-gray-600">
                                     <td class="px-3 py-2">{{ $statusIcon }}</td>
                                     <td class="px-3 py-2 font-mono text-gray-900 dark:text-white">
                                         {{ $mlb['mlb_id'] }}
