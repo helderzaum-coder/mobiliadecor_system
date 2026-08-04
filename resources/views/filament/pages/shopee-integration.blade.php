3113f14f757d7c358421502e25edf0311508aec7
Helder <helder.emiliano@hotmail.com>
2026-08-04 16:55:33 -0300

    erro ortografico

--- resources/views/filament/pages/shopee-integration.blade.php
+++ resources/views/filament/pages/shopee-integration.blade.php
@@ -28,7 +28,7 @@
 
             <div class="flex gap-3">
                 <x-filament::button wire:click="conectar" color="warning" icon="heroicon-o-arrow-top-right-on-square">
-                    {{ $this->authorized ? 'Reconectar' : 'Conectarr Shopee' }}
+                    {{ $this->authorized ? 'Reconectar' : 'Conectar Shopee' }}
                 </x-filament::button>
 
                 @if($this->authorized)
